8d892c59e226fac2f7661a9bc9025403756dae67
MusiZzzzz <135094629+MusiZzzzz@users.noreply.github.com>
2023-06-15 11:42:09 +0900

    Add files via upload

--- omikuji2/js/omikuji.js
+++ omikuji2/js/omikuji.js
@@ -2,31 +2,40 @@
 
 window.addEventListener("DOMContentLoaded",
     function(){
-        let popMsg = "<<<<<<<<やりますねー！要来力（悲）>>>>>>>";
-        window.alert(popMsg);
         $("header").textillate({
             loop: false, // ループのオンオフ
             minDisplayTime: 2000, // テキストが置き換えられるまでの表示時間
             initialDelay: 2000, // 遅延時間
             autoStart: true, // アニメーションを自動的にスタート
             in: { // フェードインのエフェクトの詳細設定
-            effect: "fadeInLeftBig", // エフェクトの名前(animate.css参照)
-            delayScale: 1.5, // 遅延時間の指数
-            delay: 50, // 文字ごとの遅延時間
-            sync: false, // trueはアニメーションをすべての文字に同時に適用
-            shuffle: true // trueは文字を順番にではなく、ランダムに
+                effect: "fadeInLeftBig", // エフェクトの名前(animate.css参照)
+                delayScale: 1.5, // 遅延時間の指数
+                delay: 50, // 文字ごとの遅延時間
+                sync: false, // trueはアニメーションをすべての文字に同時に適用
+                shuffle: true // trueは文字を順番にではなく、ランダムに
             }
-            });
-            // おみくじボタン(id="btn1") ボヤァと表示させる
-            $(function(){
+        });
+        // おみくじボタン(id="btn1") ボヤァと表示させる
+        $(function(){
             ScrollReveal().reveal("#btn1", { duration: 9000 });
-            });
-    },"5000"
+        });
+
+        // popmessage
+        setTimeout(
+            function(){
+                let popMsg = "<<<<<<<<やりますねー！要来力（悲）>>>>>>>";
+                window.alert(popMsg);
+            },
+            "4000"
+        );
+
+    },false
 
     );
 
 
     const btn1 = document.getElementById("btn1");
+    const omikujiText = document.getElementById("omikujiText");
     btn1.addEventListener("click",
     function(){
             /* let n = Math.floor(Math.random() * 5);
@@ -58,14 +67,14 @@ window.addEventListener("DOMContentLoaded",
                     btn1.style.fontSize = "15px";
                     break;
               }*/
-              btn1.style.transition ="1s";
+              omikujiText.style.transition ="1s";
               let resultText = ["大吉","吉","中吉","小吉","凶"];
               let resultColor =["#b0e0e6","#ff00ff","#ff0000","#00008b","#000000"];
               let resultFontSize =["55px","45px","40px","35px","30px",];
               let n = Math.floor(Math.random() * resultText.length);
-              btn1.textContent = resultText[n];
-              btn1.style.color = resultColor[n];
-              btn1.style.fontSize = resultFontSize[n];
+              omikujiText.textContent = resultText[n];
+              omikujiText.style.color = resultColor[n];
+              omikujiText.style.fontSize = resultFontSize[n];
         // jQueryのsnowfall
         $(document).snowfall("clear");
         $(document).ready(function(){
