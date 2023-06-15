5a9c26a5cc5574b1e18cfd6e739c5192f2362279
MusiZzzzz <135094629+MusiZzzzz@users.noreply.github.com>
2023-06-15 14:47:00 +0900

    Add files via upload

--- omikuji2/js/omikuji.js
+++ omikuji2/js/omikuji.js
@@ -32,12 +32,14 @@ window.addEventListener("DOMContentLoaded",
     },false
 
     );
-
-
+    let soundEndfalg = "0";
     const btn1 = document.getElementById("btn1");
     const omikujiText = document.getElementById("omikujiText");
     btn1.addEventListener("click",
     function(){
+        if(soundEndfalg === "1"){
+            soundControl("end","");
+        }
             /* let n = Math.floor(Math.random() * 5);
     
             switch(n){
@@ -71,10 +73,14 @@ window.addEventListener("DOMContentLoaded",
               let resultText = ["哼..哼..啊啊啊啊啊啊","哼..啊啊啊.哼..哼","哼哼..哼啊啊啊啊","哼..哼啊啊","哼.."];
               let resultColor =["#b0e0e6","#ff00ff","#ff0000","#00008b","#000000"];
               let resultFontSize =["90px","80px","70px","60px","50px","40px"];
+              let resultSound = ["sound/mus3.mp3","sound/mus1.mp2","sound/mus2.mp2","sound/mus2.mp2","sound/mus1.mp","sound/mus1.mp3",]
               let n = Math.floor(Math.random() * resultText.length);
               omikujiText.textContent = resultText[n];
               omikujiText.style.color = resultColor[n];
               omikujiText.style.fontSize = resultFontSize[n];
+              w_sound = resultSound[n];
+              soundControl("start",w_sound);
+              soundEndfalg = "1";
         // jQueryのsnowfall
         $(document).snowfall("clear");
         $(document).ready(function(){
@@ -88,3 +94,16 @@ window.addEventListener("DOMContentLoaded",
         });
         },false
     );
+
+    let w_sound
+    let music
+    function soundControl(status,w_sound){
+        if(status ==="start"){
+            music = new Audio(w_sound);
+            music.currentTime = 0;
+            music.play();
+        }else if(status === "end"){
+            music.pause();
+            music.currentTime = 0;
+        }
+    }
